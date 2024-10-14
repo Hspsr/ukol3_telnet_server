9c198dabe1f2d2f1aa8151890a150adbf6668a3e
Hspsr <119581613+Hspsr@users.noreply.github.com>
2024-10-14 21:25:22 +0200

    Update App.java

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,16 +1,35 @@
 package utb.fai;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
     public static void main(String[] args) {
-        int port = 12345;
-        int max_threads = 10;    
-
-        // Implement input parameter processing
+        int port = Integer.parseInt(args[0]);
+        int max_threads = Integer.parseInt(args[1]);
+        Socket clientSocket;
         
+        ThreadPoolExecutor threadsHandler = new ThreadPoolExecutor(max_threads, max_threads,0L,TimeUnit.MILLISECONDS,new LinkedBlockingDeque<Runnable>());
+
         // Implementation of the main server loop
-        ServerSocket serverSocket;
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+
+            while(!serverSocket.isClosed()){
+                clientSocket = serverSocket.accept();
+
+                threadsHandler.execute(new ClientThread(clientSocket));
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
+        threadsHandler.shutdown();
     }
 }
